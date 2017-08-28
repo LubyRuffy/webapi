58c8c184e62d3e74a67a8d0d74649edc3cc41dde
wanhl1990@126.com <wanhl1990@126.com>
2017-08-28 16:31:59 +0800

    Complete the create and delete action. Fix the double render problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,38 @@ class UsersController < ApplicationController
   end
 
   def create
-   # @user = User.new
-    logger.info("----params----#{require.POST()} ----")
+
+    if !current_user_is_admin?
+      api_err 20005, 'Current user can not create user.'
+      return
+    end
+
     @user = User.new(create_params)
 
+    #@user.save
     if !@user.save
       api_err 20004, 'user create error'
     end
   end
 
-  def delete
-    User.find(params[:id]).destroy
+  def destroy
+    c_user = User.find_by(username: params[:current_user])
+    if !c_user 
+      api_err 20007, 'Current user not found.'
+      return 
+    end
+
+    if c_user.role_id != 2
+      api_err 20007, 'Current user can not delete user.'
+      return 
+    end
+
+    user = User.find_by(id: params[:id])
+    if !user 
+      api_err 86, "User does not exist'"
+    elsif !user.destroy  # Use else if . Or .destroy method will not found by nil class.
+      api_err 20006, "delete user error"
+    end
   end
 
   def show
@@ -30,45 +51,57 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-
-    if !user_can_change_info? 
+    @user =  user_can_change_info? 
+    if !@user
         api_err 20002, "User info only can be changed by self or admin user"
-    end
-
-    if !@user.update_attributes(update_params)
+    elsif !@user.update_attributes(update_params)
       api_err 20003, "User info update error!"
-      #@err_code = 20003
-      #@err_msg = "User info update error!"
     end
   end
 
   private
 
   def create_params
-    require.request_parameters.permit(:username, :password, :email, :phone_no, :description, :role_id)
+    params.require(:user).permit(:username, :password, :email, :phone_no, :description, :role_id)
   end
 
   def update_params
-    params.permit(:password, :email, :phone_no, :description,:role_id)
+    params.require(:user).permit(:password, :email, :phone_no, :description,:role_id)
   end
 
+  def get_user
+    user = User.find_by(id:params[:id])
+
+    if !user
+      api_err 86, "User does not exist"
+    else
+      user
+    end
+  end
 
   # Check the user can change the info or not.
   # if the user which to change is not the user which login now, or 
   # the user logined is not the  user adminastrator. It return false.
   def user_can_change_info?
-    @user_to_modify = User.find(params[:id])
-    @user_now  = User.find_by(username: params[:user])
+    @user_to_modify = User.find_by(id:params[:id])
+    @user_now  = User.find_by(username: params[:current_user])
 
     if !@user_now
       return false
     end
 
-    if (@user_to_modify != params[:user]) && (@user_now.role_id != 2)
+    if (@user_to_modify != params[:current_user]) && (@user_now.role_id != 2)
       false
     else
-      true
+      @user_to_modify
     end
+  end
+
+  def current_user_is_admin?
+    user = User.find_by(username: params[:current_user])
+
+    return false if (!user || user.role_id != 2)
+
+    true
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   # password was passed as md5. The length maybe less than 32.  so maximum set 64.
   validates :password, presence:true, length:{minimum: 6, maximum: 64}
 
-  validates :email , length:{minimum: 7, maximum: 64}
-  validates :phone_no, length: {minimum: 7, maximum: 13}
-  validates :description, length: {maximum: 256}
+#  validates :email , length:{minimum: 7, maximum: 64}
+#  validates :phone_no, length: {minimum: 7, maximum: 13}
+#  validates :description, length: {maximum: 256}
 
   validates :role_id,  presence:true #, format: {with: }
 end
